01ec1404e8a991c23de4cc624d11a1544095fa10
Terra Vaughn <vterra28@yahoo.com>
2021-05-07 09:04:31 -0500

    working on Leaflet map

--- static/js/terra.js
+++ static/js/terra.js
@@ -1,7 +1,9 @@
+console.log("loaded main.js");
+
 // Creating map object
-var myMap = L.map("map", {
-    center: [44.9778, -93.2650],
-    zoom: 8
+var myMap = L.map("choropleth", {
+    center: [44.986656, -93.258133],
+    zoom: 13
   });
   
   // Adding tile layer
@@ -14,69 +16,71 @@ var myMap = L.map("map", {
     accessToken: API_KEY
   }).addTo(myMap);
   
-  // Load in geojson data
-  var geoData = "static/data/Median_Household_Income_2016.geojson";
+  var cityData = "static/data/Minneapolis_Neighborhoods.geojson";
+
+  L.geoJson(cityData).addTo(map);
+
+  // var geojson;
+
+
+  // // Grab data with d3
+  // d3.json(cityData).then(function(data) {
+  // // d3.json("/choropleth").then(function(data) {
   
-  var geojson;
+  //   // Create a new choropleth layer
+  //     geojson = L.choropleth(data, {
   
-  // Grab data with d3
-  d3.json(geoData).then(function(data) {
+  //     // Define what  property in the features to use
+  //     valueProperty: "inspection_score",
   
-    // Create a new choropleth layer
-    geojson = L.choropleth(data, {
+  //     // Set color scale
+  //     scale: ["#ffffb2", "#b10026"],
   
-      // Define what  property in the features to use
-      valueProperty: "MHI2016",
-  
-      // Set color scale
-      scale: ["#ffffb2", "#b10026"],
-  
-      // Number of breaks in step range
-      steps: 10,
+  //     // Number of breaks in step range
+  //     steps: 10,
   
       // q for quartile, e for equidistant, k for k-means
-      mode: "q",
-      style: {
-        // Border color
-        color: "#fff",
-        weight: 1,
-        fillOpacity: 0.8
-      },
+    //   mode: "q",
+    //   style: {
+    //     // Border color
+    //     color: "#ff1100",
+    //     weight: 1,
+    //     fillOpacity: 0.8
+    //   },
   
-      // Binding a pop-up to each layer
-      onEachFeature: function(feature, layer) {
-        layer.bindPopup("Zip Code: " + feature.properties.ZIP + "<br>Median Household Income:<br>" +
-          "$" + feature.properties.MHI2016);
-      }
-    }).addTo(myMap);
+    //   // Binding a pop-up to each layer
+    //   onEachFeature: function(feature, layer) {
+    //     layer.bindPopup("Neighborhood: " + feature.properties.neighborhood + "<br>Food Violations<br>");
+    //   }
+    // }).addTo(myMap);
   
-    // Set up the legend
-    var legend = L.control({ position: "bottomright" });
-    legend.onAdd = function() {
-      var div = L.DomUtil.create("div", "info legend");
-      var limits = geojson.options.limits;
-      var colors = geojson.options.colors;
-      var labels = [];
+  //   // Set up the legend
+  //   var legend = L.control({ position: "bottomright" });
+  //   legend.onAdd = function() {
+  //     var div = L.DomUtil.create("div", "info legend");
+  //     var limits = geojson.options.limits;
+  //     var colors = geojson.options.colors;
+  //     var labels = [];
   
-      // Add min & max
-      var legendInfo = "<h1>Median Income</h1>" +
-        "<div class=\"labels\">" +
-          "<div class=\"min\">" + limits[0] + "</div>" +
-          "<div class=\"max\">" + limits[limits.length - 1] + "</div>" +
-        "</div>";
+  //     // Add min & max
+  //     var legendInfo = "<h1>Food Inspections</h1>" +
+  //       "<div class=\"labels\">" +
+  //         "<div class=\"min\">" + limits[0] + "</div>" +
+  //         "<div class=\"max\">" + limits[limits.length - 1] + "</div>" +
+  //       "</div>";
   
-      div.innerHTML = legendInfo;
+  //     div.innerHTML = legendInfo;
   
-      limits.forEach(function(limit, index) {
-        labels.push("<li style=\"background-color: " + colors[index] + "\"></li>");
-      });
+  //     limits.forEach(function(limit, index) {
+  //       labels.push("<li style=\"background-color: " + colors[index] + "\"></li>");
+  //     });
   
-      div.innerHTML += "<ul>" + labels.join("") + "</ul>";
-      return div;
-    };
+  //     div.innerHTML += "<ul>" + labels.join("") + "</ul>";
+  //     return div;
+  //   };
   
-    // Adding legend to the map
-    legend.addTo(myMap);
+  //   // Adding legend to the map
+  //   legend.addTo(myMap);
   
-  });
+  // });
   
