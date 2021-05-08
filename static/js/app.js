6ea66c9f253ac556852f08fe7732359402477e16
Luis Gomez <luis.g.1990@gmail.com>
2021-05-08 12:36:34 -0500

    Line graph is functional now. Working using a dropdown list, need to change it to work with Terra & Joe's map.

--- static/js/app.js
+++ static/js/app.js
@@ -2,28 +2,98 @@ console.log("app.js loaded successfully!")
 
 // Luis' code
 
-// populate dataset with restaurant names
-
-var dropdownMenu = d3.select("#selRestaurant");
-
-// Read JSON then process it
-d3.json("/restaurants").then(data => {
-
-    // Gather restaurant names with address from JSON
-    var restNameAddress = data.map(rest => `${rest.business_name}-${rest.address}`);
-
-    // Gather uniques using the ... method
-    var uniqueRests = [...new Set(restNameAddress)];
+// Line Graph
+function drawLineGraph(rest, address) {
     
-    // Sort data alphabetically
-    uniqueRests.sort();
-    console.log(uniqueRests);
+    // Read JSON to extract data to plot
+    d3.json("/restaurants").then(data => {
 
-    // Push to dropdown
-    uniqueRests.forEach(rest => {
-        dropdownMenu.append("option")
-            .text(rest)
-            .property("value", rest);
+        // Filter data to name and address of restaurant to plot
+        var filteredData = data.filter(obj => obj.business_name === rest && obj.address === address);
+        
+        // Sort by date
+
+        filteredData.sort(function (x, y) {
+            let a = new Date(x.inspection_date),
+                b = new Date(y.inspection_date);
+            return a - b;
+        });
+
+        // Extract inspection dates, scores and types
+        
+        var inspDates = filteredData.map(data => data.inspection_date);
+        var inspScores = filteredData.map(data => data.inspection_score);
+        var inspTypes = filteredData.map(data => data.inspection_type); 
+
+        // Trace data for line
+        var lineData = [{
+            x: inspDates,
+            y: inspScores,
+            text: inspTypes,
+            type: 'line'
+        }];
+
+        // Layout variables
+        var lineLayout = {
+            title: `Food Inspection Scores for ${rest}`
+        };
+
+        // Draw plot
+        Plotly.newPlot("line-graph", lineData, lineLayout);
+    });
+}
+
+function initLineGraph() {
+
+
+    // populate dataset with restaurant names
+    //// Use D3 to select the dropdown menu
+    var dropdownMenu = d3.select("#selRestaurant");
+    
+    //// Read JSON then process it
+    d3.json("/restaurants").then(data => {
+
+        // Gather restaurant names with address from JSON
+        var restNameAddress = data.map(rest => `${rest.business_name} / ${rest.address}`);
+
+        // Gather uniques using the ... method
+        var uniqueRests = [...new Set(restNameAddress)];
+
+        // Sort data alphabetically
+        uniqueRests.sort();
+
+        // Push to dropdown
+        uniqueRests.forEach(rest => {
+            dropdownMenu.append("option")
+                .text(rest)
+                .property("value", rest);
+        });
+
+        var restStringArray = uniqueRests[0].split(' / ');
+        console.log(restStringArray);
+        var restName = restStringArray[0];
+        var restAddress = restStringArray[1];
+
+
+        // Update line graph
+        drawLineGraph(restName, restAddress);
+
     });
 
-});
+}
+
+// Function that refreshes line graph on change
+
+function restChanged(newRest) {
+
+    // Refresh graph using the new rest name
+    console.log(`User selected ${newRest}`);
+
+    //// Parse new restaurant info and pass to drawLineGraph
+    var restStringArray = newRest.split(' / ');
+    var restName = restStringArray[0];
+    var restAddress = restStringArray[1];
+    drawLineGraph(restName, restAddress);
+}
+
+initLineGraph();
