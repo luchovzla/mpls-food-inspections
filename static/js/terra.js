d836a65666ed8af34860dfbc14062299bcfa716d
Terra Vaughn <vterra28@yahoo.com>
2021-05-09 16:20:06 -0500

    Cleaned up cluster map

--- static/js/terra.js
+++ static/js/terra.js
@@ -28,13 +28,15 @@ d3.json("/restaurants").then(function(response) {
 
     // Set the data location property to a variable
     var location = (response[i].latitude, response[i].longitude);
+    // Extract just the year from datetime stamp
     var today = new Date();
     var year = today.getFullYear(response[i].inspection_date);
 
     // Check for location property
     if (location) {
 
-      console.log(response[i].latitude, response[i].longitude);
+      // console.log(response[i].latitude, response[i].longitude);
+      
       // Add a new marker to the cluster group and bind a pop-up
       markers.addLayer(L.marker([response[i].latitude, response[i].longitude])
         .bindPopup(response[i].business_name + "<br> Year: " + year + "<br> Inspection Type: " + response[i].inspection_type + "<br> Inspection Score: " + response[i].inspection_score));
