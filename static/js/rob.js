08c7c7a53000218b32f33112042d1d42d71086e1
ROBERTO PUPO <pupor@hotmail.com>
2021-05-09 16:05:35 -0500

    Created Apexchart

---
+++ static/js/rob.js
@@ -0,0 +1,33 @@
+console.log("rob.js is loaded");
+
+d3.json("/restaurants").then(RestaurantData => {
+
+    console.log(RestaurantData);
+});
+
+var options = {
+    series: [{
+    data: [400, 430, 448, 470, 540, 580, 690, 1100, 1200, 1380]
+  }],
+    chart: {
+    type: 'bar',
+    height: 350
+  },
+  plotOptions: {
+    bar: {
+      borderRadius: 4,
+      horizontal: true,
+    }
+  },
+  dataLabels: {
+    enabled: false
+  },
+  xaxis: {
+    categories: ['South Korea', 'Canada', 'United Kingdom', 'Netherlands', 'Italy', 'France', 'Japan',
+      'United States', 'China', 'Germany'
+    ],
+  }
+  };
+
+  var chart = new ApexCharts(document.querySelector("#bar-graph"), options);
+  chart.render();
