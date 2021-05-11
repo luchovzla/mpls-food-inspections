3a6c164b20e5adb3fe1b1533f01db45209dc6a94
Luis Gomez <luis.g.1990@gmail.com>
2021-05-11 08:54:04 -0500

    Added support for Roberto's bar graph. Slight formatting modifications.

--- static/js/rob.js
+++ static/js/rob.js
@@ -1,18 +1,43 @@
 console.log("rob.js is loaded");
+var chartRobert;
 
-d3.json("/avgScoreByNeighborhood").then(RestaurantData => {
+d3.json("/avgScoreByNeighborhood/top10routine").then(RestaurantData => {
 
 
+   let options = getOptions(RestaurantData);
+    chartRobert = new ApexCharts(document.querySelector("#bar-graph"), options);
+    chartRobert.render();
+    console.log(RestaurantData);
+});
+
+// Function that refreshes line graph on change
+
+function getOptions(RestaurantData)
+{
 
     let axisX = new Array(RestaurantData.length);
     let axisY = new Array(RestaurantData.length);
 
-    for (let index = 0; index < RestaurantData.length; index++) {
-        axisX[index] = RestaurantData[index].is_avg;
+
+    let minX = parseFloat(RestaurantData[0].is_avg);
+    let maxX = parseFloat(RestaurantData[0].is_avg);
+
+    for (let index = 0; index < RestaurantData.length; index++) {   
+   
+        var currentVal = parseFloat(RestaurantData[index].is_avg);
+        if (currentVal < minX) {
+            minX = currentVal;
+        }
+        
+        if (currentVal > maxX) {
+            maxX = currentVal;
+        }            
+        axisX[index] = currentVal;
         axisY[index] = RestaurantData[index].neighborhood;
     }
 
-    var options = {
+
+    let options = {
         series: [{
             name: "Score Average for Routine Inspection", 
             data: axisX
@@ -32,11 +57,24 @@ d3.json("/avgScoreByNeighborhood").then(RestaurantData => {
         },
         xaxis: {
             categories: axisY,
+            tickAmount: parseInt(maxX) - parseInt(minX) ,                
+            min: parseInt(minX),
+            max: parseInt(maxX),                   
         }
     };
 
-    var chart = new ApexCharts(document.querySelector("#bar-graph"), options);
-    chart.render();
+return options;
 
-    console.log(RestaurantData);
-});
+
+
+}
+
+function selectBarGraphChanged(newRest) {
+
+    d3.json("/avgScoreByNeighborhood/"+newRest).then(RestaurantData => {
+      
+        let options= getOptions(RestaurantData);        
+        chartRobert.updateOptions(options);   
+        console.log(RestaurantData);
+    });
+}
